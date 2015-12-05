1c2606f81727aa8066cf50959d1e02476a3e7ebe
Ants <anto.aravinth.cse@gmail.com>
2015-12-05 12:36:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# hello-world-angular-2
+An simple Angular 2 application without node &amp; gulp! The application is being written in Typescript and ES6 classes. 
+
+This demonstarte how to write a very simple Angular 2 application, without any noise. Open up the source file and read the components on top of 
+it to get hold of whats going on!
+
+
+###How to Run:
+Clone the project:
+
+```
+git clone https://github.com/antoaravinth/hello-world-angular-2.git
+```
+
+And then go inside the directory and run:
+
+```
+python -m SimpleHTTPServer
+```
+
+and go to `localhost:3000`. Now you will be seeing your Angular 2 application live! As simple as that.
+
+
+
---
+++ hello-world.js
@@ -0,0 +1,32 @@
+//import the Component and bootsrtap from angular fw
+import {Component, bootstrap} from 'angular2/angular2';
+
+/*
+  Placing this annotations above our
+  class turns that into an Angular component!
+
+  selector: It defines our name of our component
+            which we can use to refer in our html file. 
+
+  template : This specifies the angular component
+	     what to be displayed while its rendered.
+
+  {{}} : The name property inside the {{}} defines the data
+	 binding that our component has.
+*/
+@Component({
+  selector: 'hello-world',
+  template: `<h1>Hello {{ name }}!</h1>`
+})
+class HelloWorldComponent {
+  constructor() {
+    this.name = 'Angular 2';
+  }
+}
+
+//This will instantiates our hello-world
+//component. Note that, Angular creates the 
+//instance of our HelloWorldComponent, not 
+//by us
+bootstrap(HelloWorldComponent);
+
